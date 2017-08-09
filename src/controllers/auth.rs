7e2917f8e23a31e1f143ca8a72c48809668a8f43
Miro Dojkic <mdojkic@gmail.com>
2017-08-09 13:47:17 +0200

    implement debug for models, cleanup auth ctrl

--- src/controllers/auth.rs
+++ src/controllers/auth.rs
@@ -5,8 +5,6 @@ use iron::prelude::*;
 use iron::status;
 use iron::headers::{Authorization, Bearer};
 use iron::error::HttpError;
-use self::jwt::errors::Error;
-use self::jwt::errors::ErrorKind::{InvalidToken, InvalidIssuer};
 
 use models::auth::*;
 use utils::auth::*;
@@ -14,21 +12,18 @@ use utils::auth::*;
 pub fn login(req: &mut Request) -> IronResult<Response> {
     let user_login = req.get::<bodyparser::Struct<UserLogin>>();
 
-    let user = match user_login {
-        Ok(Some(user_login)) => Ok(match_user_credentials(&user_login)),
-        Ok(None) => Ok(None),
-        Err(e) => Err(e),
-    };
+    let user = user_login
+        .map_err(|e| IronError::new(e, status::BadRequest))?
+        .and_then(|user_login| match_user_credentials(&user_login));
 
     match user {
-        Ok(Some(user)) => {
+        Some(user) => {
             match get_jwt(&user) {
                 Ok(token) => Ok(Response::with((status::Ok, token.to_string()))),
                 Err(e) => Err(IronError::new(e, status::InternalServerError)),
             }
         }
-        Ok(None) => Ok(Response::with((status::NotFound, "Bad credentials!"))),
-        Err(e) => Err(IronError::new(e, status::BadRequest)),
+        None => Ok(Response::with((status::NotFound, "Bad credentials!"))),
     }
 }
 
@@ -43,20 +38,9 @@ pub fn authenticate(req: &mut Request) -> IronResult<()> {
         Some(authorization) => {
             match get_jwt_data(&authorization.token) {
                 Ok(_) => Ok(()),
-                Err(e) => Err(get_jwt_error_handler(e)),
+                Err(e) => Err(IronError::new(e, status::Unauthorized)),
             }
         }
-        None => Err(IronError::new(
-            HttpError::Header,
-            status::InternalServerError,
-        )),
-    }
-}
-
-fn get_jwt_error_handler(error: Error) -> IronError {
-    match *error.kind() {
-        InvalidToken => IronError::new(error, status::Unauthorized),
-        InvalidIssuer => IronError::new(error, status::Unauthorized),
-        _ => IronError::new(error, status::InternalServerError),
+        None => Err(IronError::new(HttpError::Header, status::Unauthorized)),
     }
 }
--- src/models/user.rs
+++ src/models/user.rs
@@ -1,7 +1,7 @@
 extern crate serde;
 extern crate serde_json;
 
-#[derive(Queryable, Serialize, Deserialize)]
+#[derive(Debug, Queryable, Serialize, Deserialize)]
 pub struct User {
     pub id: i32,
     pub username: String,
